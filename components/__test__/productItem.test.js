c6c7fb77025e6c3d2c1e0f733c891bcefa3ab9ac
ParmeshwarPanwar <parmeshwarpanwar777@gmail.com>
2024-10-22 22:19:59 +0530

    write test for text in productItem card

--- components/__test__/productItem.test.js
+++ components/__test__/productItem.test.js
@@ -1,32 +1,32 @@
-const { render,screen, fireEvent } = require("@testing-library/react")
+const { render, screen, fireEvent } = require("@testing-library/react")
 const { FilterContextProvider } = require("../../context/FilterContext")
 import "@testing-library/jest-dom";
 import { ToastContainer } from "react-toastify";
 import ProductItem from "../Products/productItem";
 
-describe("ProductItem Component",()=>{
+describe("ProductItem Component", () => {
     const mockProduct = {
-        id:1,
-        name:"Test Product",
-        image:"test-image.jpg",
-        price:10,
+        id: 1,
+        name: "Test Product",
+        image: "test-image.jpg",
+        price: 10,
     };
 
-    test("renders product details correctly", ()=>{
+    test("renders product details correctly", () => {
         render(
             <FilterContextProvider>
-                <ProductItem product={mockProduct}/>
+                <ProductItem product={mockProduct} />
             </FilterContextProvider>
         )
 
         expect(screen.getByText("Test Product")).toBeInTheDocument();
-        expect(screen.getByAltText("Test Product")).toHaveAttribute("src","test-image.jpg");
+        expect(screen.getByAltText("Test Product")).toHaveAttribute("src", "test-image.jpg");
     });
 
-    test("toggle wishlist status when favorite button is clicked",()=>{
+    test("toggle wishlist status when favorite button is clicked", () => {
         render(
             <FilterContextProvider>
-              <ProductItem product={mockProduct}/>
+                <ProductItem product={mockProduct} />
             </FilterContextProvider>
         )
 
@@ -40,14 +40,25 @@ describe("ProductItem Component",()=>{
         expect(favoriteButton.querySelector("svg")).toHaveStyle("color: red");
     })
 
-    test("remove product from wishlist when clicked again",() =>{
+    test("remove product from wishlist when clicked again", () => {
         render(<FilterContextProvider>
-            <ProductItem product={mockProduct}/>
+            <ProductItem product={mockProduct} />
         </FilterContextProvider>)
 
         const favoriteButton = screen.getByTestId("favoriteBtn");
         fireEvent.click(favoriteButton);
 
         expect(favoriteButton.querySelector('svg')).toHaveStyle('color: white')
+    })
+
+    test("displays correct preparation time", () => {
+        render(
+        <FilterContextProvider>
+            <ProductItem product={mockProduct} />
+        </FilterContextProvider>
+        );
+
+        expect(screen.getByText("Preparation Time: 15 mins")).toBeInTheDocument();
+
     })
 })
